d0f3fa95b80d652cf369f196eaef7744249a9d7b
Oleksii Fedorchak <ofedorchak68@gmail.com>
2020-09-08 11:55:16 +0300

    Update: rename typical function

--- src/Painter.php
+++ src/Painter.php
@@ -7,6 +7,8 @@ namespace GmailLogo;
  * this controller contain all the custom builders of some custom settings for some images.
  * it is necessary to avoid writing the same code again | storage of LogoGmail custom settings builders
  *
+ * extend this class and add more methods for your custom images.. :D
+ *
  * Class LogoTools
  * @package App\Logo
  */
@@ -15,7 +17,7 @@ class Painter
     /**
      * generate typical gmail logo..
      */
-    public function showTypicalPNG()
+    public function typical()
     {
         $temporaryLogo = new Generator('John Doe', [75, 175], Font::ROBOTO_REGULAR);
         $temporaryLogo->setHeader()
